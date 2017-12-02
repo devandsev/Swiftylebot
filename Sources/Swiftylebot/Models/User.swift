b1567e52741704539bc199eb3c1de51a81c3ab0d
Andrey Sevrikov <devandsev@gmail.com>
2017-12-03 00:21:06 +0300

    Add User model fields

--- Sources/Swiftylebot/Models/User.swift
+++ Sources/Swiftylebot/Models/User.swift
@@ -10,6 +10,19 @@ import Vapor
 
 public struct User: NodeInitializable {
     
+    public let id: Int
+    public let isBot: Bool
+    public let firstName: String
+    public let lastName: String?
+    public let username: String?
+    public let languageCode: String?
+    
     public init(node: Node) throws {
+        id = try node.get("id")
+        isBot = try node.get("is_bot")
+        firstName = try node.get("first_name")
+        lastName = try? node.get("last_name")
+        username = try? node.get("username")
+        languageCode = try? node.get("language_code")
     }
 }
