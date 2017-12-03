d382b8bd88164ea605b6bb80ab192addbfa5c4a0
Andrey Sevrikov <devandsev@gmail.com>
2017-12-03 18:57:11 +0300

    Add MesssageEntity model fields

--- Sources/Swiftylebot/Models/MessageEntity.swift
+++ Sources/Swiftylebot/Models/MessageEntity.swift
@@ -10,6 +10,40 @@ import Vapor
 
 public struct MessageEntity: NodeInitializable {
     
+    public enum MessageEntityType: String, NodeInitializable {
+        case mention = "mention"
+        case hashtag = "hashtag"
+        case botCommand = "bot_command"
+        case url = "url"
+        case email = "email"
+        case bold = "bold"
+        case italic = "italic"
+        case code = "code"
+        case pre = "pre"
+        case textLink = "text_link"
+        case textMention = "text_mention"
+        
+        public init(node: Node) throws {
+            guard let string = node.string,
+            let messageEntityType = MessageEntityType(rawValue: string) else {
+                throw NodeError.unableToConvert(input: node, expectation: "String", path: [])
+            }
+            
+            self = messageEntityType
+        }
+    }
+    
+    let type: MessageEntityType
+    let offset: Int
+    let length: Int
+    let url: String?
+    let user: User?
+    
     public init(node: Node) throws {
+        type = try node.get("type")
+        offset = try node.get("offset")
+        length = try node.get("length")
+        url = try? node.get("url")
+        user = try? node.get("user")
     }
 }
