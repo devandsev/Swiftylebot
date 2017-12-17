440e91fc5a5a1083f6d71f7ecd4e15408a7f075d
Andrey Sevrikov <devandsev@gmail.com>
2017-12-17 19:18:27 +0300

    Add ResponseParameters and UpdatesSchema's error fields

--- Sources/Swiftylebot/Models/UpdatesSchema.swift
+++ Sources/Swiftylebot/Models/UpdatesSchema.swift
@@ -10,13 +10,42 @@ import Vapor
 
 struct UpdatesSchema: NodeInitializable {
     
+    enum ResponseParameter: Int {
+        case migrateToChatId
+        case retryAfter
+    }
+    
     let ok: Bool
     let updates: [Update]
+    
+    let description: String?
+    let errorCode: String?
+    let parameters: [ResponseParameter]
     
     var lastUpdateId: Int?
     
     init(node: Node) throws {
         ok = try node.get("ok")
+        
+        guard ok else {
+            self.updates = []
+            description = try? node.get("description")
+            errorCode = try? node.get("error_code")
+            
+            var parameters: [ResponseParameter] = []
+            
+            if let migrateToChatId: ResponseParameter = try? node.get("migrate_to_chat_id") {
+                parameters.append(migrateToChatId)
+            }
+            
+            if let retryAfter: ResponseParameter = try? node.get("retry_after") {
+                parameters.append(retryAfter)
+            }
+            
+            self.parameters = parameters
+            
+            return
+        }
         
         var updates: [Update] = []
         
